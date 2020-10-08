796684dbe4a5c02fab6d5497fde96279ebd850ca
Josef Zoller <josef@walterzollerpiano.com>
2020-10-09 00:54:26 +0200

    Fix macros

--- Sources/stb/include/stb_file.h
+++ Sources/stb/include/stb_file.h
@@ -8,7 +8,7 @@
 #ifndef stb_file_h
 #define stb_file_h
 
-#ifndef STBI_NO_STDIO
+#ifndef STB_FILE_NO_STDIO
 #include <stdio.h>
 #endif
 
@@ -19,17 +19,17 @@ typedef char stb_c;
 extern "C" {
 #endif
 
-#ifndef STBDEF
-#ifdef STB_STATIC
-#define STBDEF static
+#ifndef STBFDEF
+#ifdef STBF_STATIC
+#define STBFDEF static
 #else
-#define STBDEF extern
+#define STBFDEF extern
 #endif
 #endif
 
-#ifndef STBI_NO_STDIO
-STBDEF stb_c *stb_load(char const *filename);
-STBDEF void stb_free(void *retval_from_stb_load);
+#ifndef STBF_NO_STDIO
+STBFDEF stb_c *stb_load(char const *filename);
+STBFDEF void stb_free(void *retval_from_stb_load);
 #endif
 
 
@@ -38,13 +38,13 @@ STBDEF void stb_free(void *retval_from_stb_load);
 #endif
 
 
-#ifdef STB_IMPLEMENTATION
+#ifdef STB_FILE_IMPLEMENTATION
 
 
 #ifdef __cplusplus
-#define STB_EXTERN extern "C"
+#define STBF_EXTERN extern "C"
 #else
-#define STB_EXTERN extern
+#define STBF_EXTERN extern
 #endif
 
 
@@ -55,34 +55,34 @@ typedef   signed int   stb__int32;
 typedef int32_t  stb__int32;
 #endif
 
-#if defined(STB_MALLOC) && defined(STB_FREE) && (defined(STB_REALLOC) || defined(STB_REALLOC_SIZED))
+#if defined(STBF_MALLOC) && defined(STBF_FREE) && (defined(STBF_REALLOC) || defined(STBF_REALLOC_SIZED))
 // ok
-#elif !defined(STB_MALLOC) && !defined(STB_FREE) && !defined(STB_REALLOC) && !defined(STB_REALLOC_SIZED)
+#elif !defined(STBF_MALLOC) && !defined(STBF_FREE) && !defined(STBF_REALLOC) && !defined(STBF_REALLOC_SIZED)
 // ok
 #else
-#error "Must define all or none of STBI_MALLOC, STBI_FREE, and STBI_REALLOC (or STBI_REALLOC_SIZED)."
+#error "Must define all or none of STBF_MALLOC, STBF_FREE, and STBF_REALLOC (or STBF_REALLOC_SIZED)."
 #endif
 
-#ifndef STB_MALLOC
-#define STB_MALLOC(sz)           malloc(sz)
-#define STB_REALLOC(p,newsz)     realloc(p,newsz)
-#define STB_FREE(p)              free(p)
+#ifndef STBF_MALLOC
+#define STBF_MALLOC(sz)           malloc(sz)
+#define STBF_REALLOC(p,newsz)     realloc(p,newsz)
+#define STBF_FREE(p)              free(p)
 #endif
 
-#ifndef STB_REALLOC_SIZED
-#define STB_REALLOC_SIZED(p,oldsz,newsz) STB_REALLOC(p,newsz)
+#ifndef STBF_REALLOC_SIZED
+#define STBF_REALLOC_SIZED(p,oldsz,newsz) STBF_REALLOC(p,newsz)
 #endif
 
 
-STBDEF void stb_free(void *retval_from_stbi_load)
+STBFDEF void stb_free(void *retval_from_stbi_load)
 {
-   STB_FREE(retval_from_stbi_load);
+   STBF_FREE(retval_from_stbi_load);
 }
 
 
-#ifndef STB_NO_STDIO
+#ifndef STBF_NO_STDIO
 
-#if defined(_MSC_VER) && defined(STB_WINDOWS_UTF8)
+#if defined(_MSC_VER) && defined(STBF_WINDOWS_UTF8)
 STB_EXTERN __declspec(dllimport) int __stdcall MultiByteToWideChar(unsigned int cp, unsigned long flags, const char *str, int cbmb, wchar_t *widestr, int cchwide);
 STB_EXTERN __declspec(dllimport) int __stdcall WideCharToMultiByte(unsigned int cp, unsigned long flags, const wchar_t *widestr, int cchwide, char *str, int cbmb, const char *defchar, int *used_default);
 #endif
@@ -90,7 +90,7 @@ STB_EXTERN __declspec(dllimport) int __stdcall WideCharToMultiByte(unsigned int 
 static FILE *stb__fopen(char const *filename, char const *mode)
 {
    FILE *f;
-#if defined(_MSC_VER) && defined(STB_WINDOWS_UTF8)
+#if defined(_MSC_VER) && defined(STBF_WINDOWS_UTF8)
    wchar_t wMode[64];
    wchar_t wFilename[1024];
     if (0 == MultiByteToWideChar(65001 /* UTF8 */, 0, filename, -1, wFilename, sizeof(wFilename)))
@@ -115,7 +115,7 @@ static FILE *stb__fopen(char const *filename, char const *mode)
    return f;
 }
 
-STBDEF stb_c *stb_load(char const *filename)
+STBFDEF stb_c *stb_load(char const *filename)
 {
     stb_c * buffer;
     stb__int32 length;
@@ -127,7 +127,7 @@ STBDEF stb_c *stb_load(char const *filename)
         length = ftell(f);
         fseek(f, 0, SEEK_SET);
 
-        buffer = (stb_c *)STB_MALLOC(length + 1);
+        buffer = (stb_c *)STBF_MALLOC(length + 1);
         fread(buffer, 1, length, f);
         buffer[length] = 0;
 
@@ -137,9 +137,9 @@ STBDEF stb_c *stb_load(char const *filename)
     return buffer;
 }
 
-#endif //!STB_NO_STDIO
+#endif //!STBF_NO_STDIO
 
-#endif // STB_IMPLEMENTATION
+#endif // STB_FILE_IMPLEMENTATION
 
 
 #endif /* stb_file_h */
--- Sources/stb/stb.cpp
+++ Sources/stb/stb.cpp
@@ -5,6 +5,6 @@
 //  Created by Josef Zoller on 06.10.20.
 //
 
-#define STB_IMPLEMENTATION
+#define STB_FILE_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb.h"
